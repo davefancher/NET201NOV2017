24d4c554af2a8dce9a94f77c2b1036f529ef1cfa
Dave Fancher <dave@davefancher.com>
2017-11-16 14:30:26 -0500

    Added edit and delete

--- ElevenNote.Web/Controllers/NotesController.cs
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -54,5 +54,58 @@ namespace ElevenNote.Web.Controllers
 
             return View(model);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var detailModel = CreateNoteService().GetNoteById(id);
+            var editModel =
+                new NoteEditModel
+                {
+                    NoteId = detailModel.NoteId,
+                    Title = detailModel.Title,
+                    Content = detailModel.Content
+                };
+
+            return View(editModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEditModel model)
+        {
+            if(model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Nice try!");
+                model.NoteId = id;
+                return View(model);
+            }
+
+            if (!ModelState.IsValid) return View(model);
+
+            if(!CreateNoteService().UpdateNote(model))
+            {
+                ModelState.AddModelError("", "Unable to update note");
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Delete")]
+        public ActionResult DeleteGet(int id)
+        {
+            var model = CreateNoteService().GetNoteById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            CreateNoteService().DeleteNote(id);
+
+            return RedirectToAction("Index");
+        }
     }
 }
